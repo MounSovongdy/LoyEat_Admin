3e0016b3568312cb79cb69d6396e67e11a655994
Moun Sovongdy <mounsovongdy1234567@gmail.com>
2022-08-15 20:18:17 +0700

    firebase

--- lib/src/srceen/landing_screen.dart
+++ lib/src/srceen/landing_screen.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:loyeat_admin/src/widget/bottom_app_bar_widget.dart';
 import 'package:loyeat_admin/src/widget/button_submit-widget.dart';
 import 'package:loyeat_admin/src/widget/text_field_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LandingScreen extends StatelessWidget {
-  LandingScreen({Key? key}) : super(key: key);
+  CollectionReference customers =
+      FirebaseFirestore.instance.collection('customers');
+      
+        get customer_name => null;
+
+  // ignore: non_constant_identifier_names
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +27,55 @@ class LandingScreen extends StatelessWidget {
 
   Widget get body {
     return ListView(
-      children: const [
-        TextFieldWidget(),
-        ButtonSubmitWidget(),
+      children: [
+        text_field,
+        button_submit,
+        //TextFieldWidget(),
       ],
+    );
+  }
+
+  Widget get text_field {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            child: TextField(
+              onChanged: (value) {
+                String customer_name;
+                customer_name = value;
+              },
+              obscureText: false,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                labelText: 'Customer Name',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget get button_submit {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 8),
+      child: TextButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        ),
+        onPressed: () async {
+          await customers.add({'customer_name': customer_name}).then(
+              (value) => print('Added'));
+        },
+        child: const Text('Submit'),
+      ),
     );
   }
 }
--- lib/src/widget/text_field_widget.dart
+++ lib/src/widget/text_field_widget.dart
@@ -1,133 +1,136 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-class TextFieldWidget extends StatelessWidget {
-  const TextFieldWidget({Key? key}) : super(key: key);
+// class TextFieldWidget extends StatelessWidget {
+//   const TextFieldWidget({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Customer ID',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Customer Name',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Customer Rating',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Marchent ID',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Marchant Name',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Marchant Rating'),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Order ID',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Distance',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Delivery Fee',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Date',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Time Minute',
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: const TextField(
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Time',
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   String cusName;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Column(
+//         children: [
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Customer ID',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Customer Name',
+//               ),
+              
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Customer Rating',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Marchent ID',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Marchant Name',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                   border: OutlineInputBorder(), labelText: 'Marchant Rating'),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Order ID',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Distance',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Delivery Fee',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Date',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Time Minute',
+//               ),
+//             ),
+//           ),
+//           Container(
+//             padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+//             child: const TextField(
+//               obscureText: false,
+//               decoration: InputDecoration(
+//                 border: OutlineInputBorder(),
+//                 labelText: 'Time',
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
